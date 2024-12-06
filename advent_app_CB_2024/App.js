dce95bb945ddb3fdd3c869d9a3174fc7088d8e29
F3rndal <n.banek@outlook.de>
2024-12-06 15:08:03 +0100

    validation for different input fields

--- advent_app_CB_2024/App.js
+++ advent_app_CB_2024/App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import {TextInput, Button, StyleSheet, Text, View } from 'react-native';
+import {Alert, TextInput, Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
 
   const [name, setName] = useState('');
   const [age, setAge] = useState('');
-  const [adress, setAdress] = useState('');
+  const [address, setAddress] = useState('');
+
+  const validateName = (text) => {
+    const regex = /^[a-zA-Z\s]*$/; 
+    if (regex.test(text)) {
+      setName(text);
+    } else {
+      Alert.alert('Unvalid Name', 'Ho ho ho what an odd name. Try it again without numbers or special characters');
+    }
+  };
+
+  const validateAge = (text) => {
+    const regex = /^[1-9][0-9]?$/; 
+    if (regex.test(text) || text === '') {
+      setAge(text);
+    } else {
+      Alert.alert('Unvalid Age', 'We have a strict age policy between 1-99');
+    }
+  };
+
+  const validateAddress = (text) => {
+    const regex = /^[a-zA-Z0-9\s]*$/; 
+    if (regex.test(text)) {
+      setAddress(text);
+    } else {
+      Alert.alert('Unvalid Address', 'Sorry but Rudolf has a special characters alergy.');
+    }
+  };
+
 
   return (
     <View style={styles.container}>
@@ -16,20 +44,21 @@ export default function App() {
         style={styles.input}
         placeholder="Enter your Name"
         value={name}
-        onChangeText={(text) => setName(text)}
+        onChangeText={validateName}
       /> 
 
       <TextInput
         style={styles.input}
         placeholder="Enter your Age"
         value={age}
-        onChangeText={(text) => setAge(text)}
+        onChangeText={validateAge}
+        keyboardType='numeric'
       /> 
       <TextInput
         style={styles.input}
-        placeholder="Enter your Adress"
-        value={adress}
-        onChangeText={(text) => setAdress(text)}
+        placeholder="Enter your Address"
+        value={address}
+        onChangeText={validateAddress}
       /> 
       
       
